e114f21f1bb687427d2fe9e3d9957e94d3f1bb80
bmcilhenny <bmcilhen@gmail.com>
2019-01-08 00:31:21 -0500

    commented out provider, createStore, now created in Root component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from 'reducers';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
+// import reducers from 'reducers';
 // ^^ works cause it assumes index.js when we import directory
-
+// we don't need this anymore cause of Root component handles all that, our tests will now work as long as we wrap our components in tests files with Root
+import Root from 'Root';
 import App from 'components/App';
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, {})}>
+  <Root>
     <App />
-  </Provider>
+  </Root>
   , document.querySelector('#root'))
