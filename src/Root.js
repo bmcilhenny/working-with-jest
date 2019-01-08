18d3b4687a3a5b8fbd447abd30ab126e6cb483f9
bmcilhenny <bmcilhen@gmail.com>
2019-01-08 16:15:45 -0500

    add test to make sure lis are rendered and comments are displayed, add optional initial state to Root component

--- src/Root.js
+++ src/Root.js
@@ -3,10 +3,20 @@ import {Provider } from 'react-redux';
 import {createStore } from 'redux';
 import reducers from 'reducers';
 
-export default (props) => {
+// export default (props) => {
+//   return (
+//     <Provider store={createStore(reducers, props.initialState)}>
+//       {props.children}
+//       {/* react construct allows us to take the component we create and wrap other components  */}
+//     </Provider>
+//   )
+// }
+
+// object destructuring courtesy of ES6
+export default ({children, initialState = {}}) => {
   return (
-    <Provider store={createStore(reducers, {})}>
-      {props.children}
+    <Provider store={createStore(reducers, initialState)}>
+      {children}
       {/* react construct allows us to take the component we create and wrap other components  */}
     </Provider>
   )
--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -5,17 +5,23 @@ import Root from 'root';
 
 let wrapped;
 beforeEach(() => {
+
+  const initialState = {
+    comments: ['Hey there bingo', 'WooO Bessy, settle down!']
+  };
   wrapped = mount(
-    <Root>
+    <Root initialState={initialState}>
       <CommentList />
     </Root>
   )
 })
 
 it('creates one LI for each comment', () => {
-
+  // console.log(wrapped.find('li').length);
+  expect(wrapped.find('li').length).toEqual(2);
 });
 
 it('text from each comment is visible', () => {
-  
+  expect(wrapped.render().text()).toContain('Hey there bingo');
+  expect(wrapped.render().text()).toContain('WooO Bessy, settle down!');
 })
