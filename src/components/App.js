253af1843f7e7e5ffe87ca65309a9dd33bfd47fb
bmcilhenny <bmcilhen@gmail.com>
2019-01-08 23:28:01 -0500

    refactor App to class-based component for adding helpers, add ternary for log in log out button based on auth state in store

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
 import React, {Fragment} from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
-const App = () => {
-  return (
-    <Fragment>
-      <Route path='/post' component={CommentBox}/>
-      <Route path='/' exact component={CommentList}/>
-    </Fragment>
-  );
-};
+class App extends React.Component {
 
-export default App;
+  renderButton() {
+    return this.props.auth ? <button>Log out</button> : <button>Log In</button>
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <Fragment>
+      {this.renderHeader()}
+        <Route path='/post' component={CommentBox}/>
+        <Route path='/' exact component={CommentList}/>
+      </Fragment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {auth: state.auth}
+}
+
+export default connect(mapStateToProps, null)(App);
